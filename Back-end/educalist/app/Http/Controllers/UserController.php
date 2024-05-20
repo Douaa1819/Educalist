20bde4d67ae97acdb0b1b5245ea410a834700cae
Douaa <douaachemnae@gmail.com>
2024-05-20 17:10:40 +0100

    logout is Done

--- Back-end/educalist/app/Http/Controllers/UserController.php
+++ Back-end/educalist/app/Http/Controllers/UserController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function logout()
+    {
+        /** @disregard P1013 [tokens function exists and working] **/
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'logged out'
+        ];
+    }
 }
